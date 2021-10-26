9c9314f36b96da820175ecf59f859e73cdeb1a8c
WhiteFox <53935741+WhiteFox2232@users.noreply.github.com>
2021-10-27 01:25:52 +0200

    🔨Add Cool "Login in as" and Not Permission message

--- client.js
+++ client.js
@@ -7,7 +7,11 @@ for (let i = 0; i < config.token.length; ++i) {
 
     let client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
     client.once('ready', () => {
-      console.log(`[I]Bot ` + i + ` Started[I]`); // Bot identity
+      console.log(`
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+│    Logged in as ${client.user.tag} ==> ✔️        │
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛`);
+
       client.user.setActivity(config.activity.message, {
         type: config.activity.type,
       });
@@ -16,15 +20,14 @@ for (let i = 0; i < config.token.length; ++i) {
     client.on('messageCreate', msg => {
       if (msg.content.startsWith(config.command)) {
         if (msg.author.id !== config.whitelist)
-            return;
-        console.log(`[!].spam detected[!]`);
+            return msg.reply("Vous n'avez pas la permission");
+        console.log(`Spam Command Detected`);
         let yourmessage = msg.content.slice(config.command.length); 
         var i;
         
         for (i = 0; i < config.spam_number; i++) {
             setTimeout(() => {
                 msg.channel.send(yourmessage)
-                console.log(`Spam Send`); 
             }, config.delay)  
         }
       }
@@ -32,18 +35,19 @@ for (let i = 0; i < config.token.length; ++i) {
 
     client.on('messageCreate', msg => {
       if (msg.content === '.stop') {
+        console.log(`Stop Command Detected `);
         if (msg.author.id !== config.whitelist)
-            return;
+            return msg.reply("Vous n'avez pas la permission");
         msg.delete()
         process.exit(1);
       }
 
       if (msg.content.startsWith(config.commandMP)) {
         if (msg.author.id !== config.whitelist)
-            return;
+            return msg.reply("Vous n'avez pas la permission");
         let messageArray = msg.content.split(' ')
         let args = messageArray.splice(1)
-        console.log('[!].spam detected[!]');
+        console.log(`Spam DM Command Detected`);
         let yourmessage2 = args.slice(1).join(" ");
         let user = msg.mentions.members.first() || msg.guild.members.cache.get(args[0])
         var i;
@@ -51,7 +55,6 @@ for (let i = 0; i < config.token.length; ++i) {
         for (i = 0; i < config.spam_number; i++) { 
             setTimeout(() => {
                 user.send(yourmessage2)
-                console.log('Spam DM Send');
             }, config.delay) 
         }
       }
